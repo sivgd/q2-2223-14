b319c89e5cf63758f9f2b01b94db69dbfee69594
MUKILTEO\1713514 <1713514@mukilteo.wednet.edu>
2023-01-10 10:25:00 -0800

    bug fixes and final touches

--- Team 14 Q2 Project/Assets/Aldo/GameManagerScript.cs
+++ Team 14 Q2 Project/Assets/Aldo/GameManagerScript.cs
@@ -10,24 +10,12 @@ public class GameManagerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+       // Cursor.visible = false;
+       // Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (gameOverUI.activeInHierarchy)
-        {
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
-        }
-        else
-        {
-            Cursor.visible = false;
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-    }
+    
 
     public void gameOver()
     {
