60284bbd133f911825575c622ccd0909126ff2c4
MUKILTEO\1714222 <MUKILTEO\1714222>
2023-01-19 09:08:14 -0800

    Making Snake patrol

--- Team 14 Q2 Project/Assets/Ian/Enemy Animations new/Patrolbehaviour.cs
+++ Team 14 Q2 Project/Assets/Ian/Enemy Animations new/Patrolbehaviour.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 public class Patrolbehaviour : MonoBehaviour
 {
     public float speed;
-    public float ratDist;
+    public float rayDist;
     private bool movingRight;
     public Transform groundDetect;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+ 
     void Update()
     {
-        
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundCheck = Physics2D.Raycast(groundDetect.position, Vector2.down, rayDist);
+
+        if (groundCheck.collider == false)
+        {
+            if (movingright)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
     }
 }
