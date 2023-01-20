0e769cda2637c1d94596e7892cd0e94a46caaa38
MUKILTEO\1713514 <1713514@mukilteo.wednet.edu>
2023-01-20 09:10:53 -0800

    Start Screen Changes/Ui fixes

---
+++ Team 14 Q2 Project/Assets/Aldo/SceneChange.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+   void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Title Screen");
+
+            Debug.Log("Space");
+        }
+    }
+
+}
